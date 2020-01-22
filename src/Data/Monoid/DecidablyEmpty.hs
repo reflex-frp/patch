82c1cd150666c0ad42aa3a935f469b7c571e73d2
John Ericson <John.Ericson@Obsidian.Systems>
2020-01-22 16:47:09 -0500

    Add DecidablyEmpty instance for Proxy

--- src/Data/Monoid/DecidablyEmpty.hs
+++ src/Data/Monoid/DecidablyEmpty.hs
@@ -3,7 +3,12 @@
 -- TODO upstream somwhere else?
 module Data.Monoid.DecidablyEmpty where
 
+import Data.Proxy
+
 class Monoid a => DecidablyEmpty a where
   isEmpty :: a -> Bool
   default isEmpty :: Eq a => a -> Bool
   isEmpty = (==) mempty
+
+instance DecidablyEmpty (Proxy a) where
+  isEmpty ~Proxy = True
