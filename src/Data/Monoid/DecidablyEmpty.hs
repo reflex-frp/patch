f39c676bc8e6bea39d7a52f5ab2e7e34e6f501a2
John Ericson <John.Ericson@Obsidian.Systems>
2020-01-22 14:40:50 -0500

    Add default method for DecidablyEmpty.isEmpty

--- src/Data/Monoid/DecidablyEmpty.hs
+++ src/Data/Monoid/DecidablyEmpty.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DefaultSignatures #-}
+
 -- TODO upstream somwhere else?
 module Data.Monoid.DecidablyEmpty where
 
 class Monoid a => DecidablyEmpty a where
   isEmpty :: a -> Bool
+  default isEmpty :: Eq a => a -> Bool
+  isEmpty = (==) mempty
